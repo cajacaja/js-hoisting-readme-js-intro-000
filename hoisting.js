70e1f6b01f53de46b161f25f6330b88859a07a51
Emir Cajic <emircajic@yahoo.com>
2019-02-22 10:24:43 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -22,11 +22,12 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+    
+  }
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+  
 }
